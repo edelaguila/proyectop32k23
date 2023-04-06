b6538879aaa17264eaa3bbb7f4974466685a324d
Bryan Illescas <98113349+BryanIllescas@users.noreply.github.com>
2023-04-06 12:31:04 -0600

    arreglos de acuerdo a parametros

--- Proyectop32k23/src/main/java/Seguridad/Vista/frmMantenimientoAplicacion.java
+++ Proyectop32k23/src/main/java/Seguridad/Vista/frmMantenimientoAplicacion.java
@@ -37,7 +37,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         clsAplicacion aplicacion = new clsAplicacion();
         //VendedorDAO vendedorDAO = new VendedorDAO();
         List<clsAplicacion> listaAplicaciones = aplicacion.getListadoAplicaciones();
-        tablaUsuarios.setModel(modelo);
+        tablaAplicaciones.setModel(modelo);
         String[] dato = new String[3];
         for (int i = 0; i < listaAplicaciones.size(); i++) {
             dato[0] = Integer.toString(listaAplicaciones.get(i).getIdAplicacion());
@@ -67,19 +67,19 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         btnEliminar = new javax.swing.JButton();
         btnRegistrar = new javax.swing.JButton();
         btnBuscar = new javax.swing.JButton();
-        label1 = new javax.swing.JLabel();
+        lbNombreTabla = new javax.swing.JLabel();
         btnModificar = new javax.swing.JButton();
-        label3 = new javax.swing.JLabel();
-        txtbuscado = new javax.swing.JTextField();
+        lbNombre = new javax.swing.JLabel();
+        txtbuscar = new javax.swing.JTextField();
         txtNombre = new javax.swing.JTextField();
         btnLimpiar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        tablaUsuarios = new javax.swing.JTable();
-        txtContrasena = new javax.swing.JTextField();
-        label5 = new javax.swing.JLabel();
+        tablaAplicaciones = new javax.swing.JTable();
+        txtEstatus = new javax.swing.JTextField();
+        lbEstatus = new javax.swing.JLabel();
         lb = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        label4 = new javax.swing.JLabel();
+        btnAyuda = new javax.swing.JButton();
+        lbBuscarID = new javax.swing.JLabel();
         btnActualizar = new javax.swing.JButton();
 
         lb2.setForeground(new java.awt.Color(204, 204, 204));
@@ -113,8 +113,8 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
             }
         });
 
-        label1.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label1.setText("Aplicaciones");
+        lbNombreTabla.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        lbNombreTabla.setText("Aplicaciones");
 
         btnModificar.setText("Modificar");
         btnModificar.addActionListener(new java.awt.event.ActionListener() {
@@ -123,12 +123,11 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
             }
         });
 
-        label3.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label3.setText("Nombre");
+        lbNombre.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        lbNombre.setText("Nombre");
 
         txtNombre.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
         txtNombre.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        txtNombre.setOpaque(false);
 
         btnLimpiar.setText("Limpiar");
         btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
@@ -137,44 +136,43 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
             }
         });
 
-        tablaUsuarios.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        tablaUsuarios.setModel(new javax.swing.table.DefaultTableModel(
+        tablaAplicaciones.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        tablaAplicaciones.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
             new String [] {
-                "ID Vendedor", "ID Empleado", "Correo", "Telefono", "Direccion", "Porcentaje", "Comision"
+                "ID Aplicacion", "Nombre Aplicacion", "Estatus Aplicacion"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, true, true, true
+                false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(tablaUsuarios);
+        jScrollPane1.setViewportView(tablaAplicaciones);
 
-        txtContrasena.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
-        txtContrasena.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
-        txtContrasena.setOpaque(false);
+        txtEstatus.setFont(new java.awt.Font("Century Gothic", 0, 12)); // NOI18N
+        txtEstatus.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 1, 0, new java.awt.Color(204, 204, 204)));
 
-        label5.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label5.setText("Estatus");
+        lbEstatus.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        lbEstatus.setText("Estatus");
 
         lb.setForeground(new java.awt.Color(204, 204, 204));
         lb.setText(".");
 
-        jButton2.setText("Ayuda");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnAyuda.setText("Ayuda");
+        btnAyuda.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnAyudaActionPerformed(evt);
             }
         });
 
-        label4.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
-        label4.setText("ID a buscar");
+        lbBuscarID.setFont(new java.awt.Font("Century Gothic", 1, 12)); // NOI18N
+        lbBuscarID.setText("ID a buscar");
 
         btnActualizar.setText("Actualizar");
         btnActualizar.addActionListener(new java.awt.event.ActionListener() {
@@ -192,20 +190,20 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(label3)
-                            .addComponent(label5))
+                            .addComponent(lbNombre)
+                            .addComponent(lbEstatus))
                         .addGap(29, 29, 29)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtContrasena, javax.swing.GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)
+                            .addComponent(txtEstatus, javax.swing.GroupLayout.DEFAULT_SIZE, 263, Short.MAX_VALUE)
                             .addComponent(txtNombre))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(lb, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                         .addGroup(layout.createSequentialGroup()
                             .addContainerGap()
-                            .addComponent(label4)
+                            .addComponent(lbBuscarID)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(txtbuscado, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGap(18, 18, 18)
                             .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
@@ -218,7 +216,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
                                 .addGroup(layout.createSequentialGroup()
                                     .addComponent(btnLimpiar, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addComponent(btnAyuda, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
                             .addGap(14, 14, 14)
                             .addComponent(btnEliminar, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(19, 19, 19)
@@ -227,7 +225,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
                         .addComponent(btnActualizar)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(label1)
+                        .addComponent(lbNombreTabla)
                         .addGap(294, 562, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
@@ -236,7 +234,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(label1)
+                .addComponent(lbNombreTabla)
                 .addGap(4, 4, 4)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -246,11 +244,11 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
                                 .addGap(45, 45, 45)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(label3))
+                                    .addComponent(lbNombre))
                                 .addGap(18, 18, 18)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(label5)))
+                                    .addComponent(txtEstatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(lbEstatus)))
                             .addComponent(lb))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -260,12 +258,12 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
                         .addGap(3, 3, 3)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btnLimpiar)
-                            .addComponent(jButton2))
+                            .addComponent(btnAyuda))
                         .addGap(5, 5, 5)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(btnBuscar)
-                            .addComponent(txtbuscado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(label4))))
+                            .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lbBuscarID))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(btnActualizar)
                 .addContainerGap(46, Short.MAX_VALUE))
@@ -278,7 +276,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
         int registrosBorrados=0;
         clsAplicacion aplicacion = new clsAplicacion();
-        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscado.getText()));
+        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscar.getText()));
         registrosBorrados=aplicacion.setBorrarAplicacion(aplicacion);
         JOptionPane.showMessageDialog(null, "Registro Borrado\n", 
                     "Información del Sistema", JOptionPane.INFORMATION_MESSAGE);
@@ -289,9 +287,9 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
     private void btnRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarActionPerformed
         clsAplicacion aplicacion = new clsAplicacion();
         aplicacion.setNombreAplicacion(txtNombre.getText());
-        aplicacion.setEstatusAplicacion(txtContrasena.getText());
+        aplicacion.setEstatusAplicacion(txtEstatus.getText());
         aplicacion.setIngresarAplicacion(aplicacion);
-        JOptionPane.showMessageDialog(null, "Registro Ingresado\n", 
+        JOptionPane.showMessageDialog(null, "RegistrtxtEstatus", 
                     "Información del Sistema", JOptionPane.INFORMATION_MESSAGE);
         llenadoDeTablas();
         limpiarTextos();
@@ -301,21 +299,21 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         // TODO add your handling code here:
         clsAplicacion aplicacion = new clsAplicacion();
         //aplicacion.setNombreAplicacion(txtbuscado.getText());        
-        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscado.getText()));        
+        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscar.getText()));        
         aplicacion = aplicacion.getBuscarInformacionAplicacionPorId(aplicacion);
         System.out.println("Usuario retornado:" + aplicacion);        
         txtNombre.setText(aplicacion.getNombreAplicacion());
-        txtContrasena.setText(aplicacion.getEstatusAplicacion());
+        txtEstatus.setText(aplicacion.getEstatusAplicacion());
     }//GEN-LAST:event_btnBuscarActionPerformed
 
-    private void btnModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
+    private void btnModifitxtEstatusormed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarActionPerformed
 //        // TODO add your handling code here:
         clsAplicacion aplicacion = new clsAplicacion();
-        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscado.getText()));
+        aplicacion.setIdAplicacion(Integer.parseInt(txtbuscar.getText()));
         aplicacion.setNombreAplicacion(txtNombre.getText());
-        aplicacion.setEstatusAplicacion(txtContrasena.getText());
+        aplicacion.setEstatusAplicacion(txtEstatus.getText());
         aplicacion.setModificarAplicacion(aplicacion);
-        JOptionPane.showMessageDialog(null, "Registro Modificado\n", 
+        JOptionPane.showMessageDialog(null, "RegisttxtEstatus\n", 
                     "Información del Sistema", JOptionPane.INFORMATION_MESSAGE);        
         llenadoDeTablas();
         limpiarTextos();
@@ -329,12 +327,12 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
     public void limpiarTextos()
     {
         txtNombre.setText("");
-        txtContrasena.setText("");
-        txtbuscado.setText("");
+        txtEstatus.setText("");
+        txtbuscar.setText("");
     }
     public void habilitarBotones()
     {
-        btnRegistrar.setEnabled(true);
+        btnRegistrar.setEnabled(true);        
         btnModificar.setEnabled(true);
         btnEliminar.setEnabled(true);
     }
@@ -345,7 +343,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         btnEliminar.setEnabled(false);
     }    
     
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnAyudaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAyudaActionPerformed
         // TODO add your handling code here:
         try {
             if ((new File("src\\main\\java\\ayudas\\ProcesoMayor.chm")).exists()) {
@@ -360,7 +358,7 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btnAyudaActionPerformed
 
     private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
         // TODO add your handling code here:
@@ -370,23 +368,23 @@ public class frmMantenimientoAplicacion extends javax.swing.JInternalFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnActualizar;
+    private javax.swing.JButton btnAyuda;
     private javax.swing.JButton btnBuscar;
     private javax.swing.JButton btnEliminar;
     private javax.swing.JButton btnLimpiar;
     private javax.swing.JButton btnModificar;
     private javax.swing.JButton btnRegistrar;
-    private javax.swing.JButton jButton2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JLabel label1;
-    private javax.swing.JLabel label3;
-    private javax.swing.JLabel label4;
-    private javax.swing.JLabel label5;
     private javax.swing.JLabel lb;
     private javax.swing.JLabel lb2;
+    private javax.swing.JLabel lbBuscarID;
+    private javax.swing.JLabel lbEstatus;
+    private javax.swing.JLabel lbNombre;
+    private javax.swing.JLabel lbNombreTabla;
     private javax.swing.JLabel lbusu;
-    private javax.swing.JTable tablaUsuarios;
-    private javax.swing.JTextField txtContrasena;
+    private javax.swing.JTable tablaAplicaciones;
+    private javax.swing.JTextField txtEstatus;
     private javax.swing.JTextField txtNombre;
-    private javax.swing.JTextField txtbuscado;
+    private javax.swing.JTextField txtbuscar;
     // End of variables declaration//GEN-END:variables
 }
