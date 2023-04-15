01c67c6eb4a52ed6f6940a70b120a3e7e4fbaa19
Alan Galicia <agaliciar4@miumg.edu.gt>
2023-04-14 23:15:11 -0600

    subiendo el llenado de tablas 1 y 2 en la clase frmProcesoAplicacionPerfil

--- Proyectop32k23/src/main/java/Seguridad/Vista/frmProcesoAplicacionPerfil.java
+++ Proyectop32k23/src/main/java/Seguridad/Vista/frmProcesoAplicacionPerfil.java
@@ -22,6 +22,8 @@ public class frmProcesoAplicacionPerfil extends javax.swing.JInternalFrame {
      */
     public frmProcesoAplicacionPerfil() {
         initComponents();
+        llenadoDeTabla1();
+        llenadoDeTabla2();
     }
 
     /**
