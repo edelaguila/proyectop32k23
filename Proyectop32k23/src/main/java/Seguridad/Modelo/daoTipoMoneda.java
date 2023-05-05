9883998d56b44e0e5685ec1280e3be68ddc2be0d
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-05-05 09:40:53 -0600

    conexion base de datos agregado al dao tipo moneda

--- Proyectop32k23/src/main/java/Seguridad/Modelo/daoTipoMoneda.java
+++ Proyectop32k23/src/main/java/Seguridad/Modelo/daoTipoMoneda.java
@@ -14,3 +14,13 @@ import java.util.List;
  *
  * @author visitante
  */
+public class daoTipoMoneda {
+
+    private static final String SQL_SELECT = "SELECT tipModId, tipMondNombre, tipMondAbreviacion, tipModValor FROM tbl_tipoMoneda";
+    private static final String SQL_INSERT = "INSERT INTO tbl_tipoMoneda(tipModId, tipMondNombre, tipMondAbreviacion, tipModValor) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE tbl_tipoMoneda SET tipMondNombre=?, tipMondAbreviacion=?,  tipModValor=?  WHERE tipModId = ?";
+    private static final String SQL_DELETE = "DELETE FROM tbl_tipoMoneda WHERE tipModId=?";
+    private static final String SQL_SELECT_NOMBRE = "SELECT tipModId, tipMondNombre, tipMondAbreviacion, tipModValor  FROM tbl_tipoMoneda WHERE tipMondNombre = ?";
+    private static final String SQL_SELECT_ID = "SELECT tipModId, tipMondNombre, tipMondAbreviacion, tipModValor  FROM tbl_tipoMoneda WHERE tipModId = ?";     
+
+
