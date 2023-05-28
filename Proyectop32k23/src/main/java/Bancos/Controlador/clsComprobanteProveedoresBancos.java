edf7c129d435026082e9117c0f7b4786ce46a84e
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-05-28 01:15:45 -0600

    variables agregadas al cls

---
+++ Proyectop32k23/src/main/java/Bancos/Controlador/clsComprobanteProveedoresBancos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Hecho por: Karla Sofia Góomez Tobar 9959-21-1896
+package Bancos.Controlador;
+import java.util.List;
+import Bancos.Modelo.daoComprobanteProveedoresBancos;
+/**
+ *
+ * @author visitante
+ */
+public class clsComprobanteProveedoresBancos {
+    private int comId;
+    private String conFechaEmision;
+    private int tipMovid;
+    private int cueId;
+    private int concId ;
+    private int codBanco;
+    private int movDetId;
+    private int cpid ;
