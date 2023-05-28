5de36c850bbf530b986f5ffb2b5177bdc7b55100
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-05-28 01:21:21 -0600

    import creados en el dao

---
+++ Proyectop32k23/src/main/java/Bancos/Modelo/daoComprobanteProveedoresBancos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Hecho por: Karla Sofia Góomez Tobar 9959-21-1896
+package Bancos.Modelo;
+
+import Bancos.Controlador.clsComprobanteProveedoresBancos;
+import Seguridad.Modelo.Conexion;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author visitante
+ */
