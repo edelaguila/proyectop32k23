3e2e42312237f692db2a8487405789aee8bc6f0c
Alan Galicia <agaliciar4@miumg.edu.gt>
2023-06-02 12:58:49 -0600

    subiendo problema de conexión arreglado

--- Proyectop32k23/src/main/java/Seguridad/Modelo/Conexion.java
+++ Proyectop32k23/src/main/java/Seguridad/Modelo/Conexion.java
@@ -6,15 +6,8 @@ public class Conexion {
 
 private static final String JDBC_URL = "jdbc:mysql://localhost/proyectop312023?useSSL=false&serverTimezone=UTC";    
 
-<<<<<<< HEAD
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "Mariano3632";
-  
-=======
-    private static final String JDBC_USER = "UsuPrueba";
-    private static final String JDBC_PASS = "123456";
-
->>>>>>> 5c69f3f4fbc07cde905c19c8877e26fe5693e55a
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
     }
